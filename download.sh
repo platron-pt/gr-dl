04e9ec618321064ce463f9fd4f8fd790e25e28a9
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2023-12-17 16:37:27 +0800

    use aria2 to download file

---
+++ download.sh
@@ -0,0 +1,22 @@
+function completed(){
+    echo Status: Completed > ../info.txt
+    mv * ../output
+}
+
+function failed(){
+    echo Status: Failed > ../info.txt
+    touch ../output/failed
+}
+cd download
+
+aria2c $1
+
+if [ "$?" -eq "0" ]
+then
+    completed
+else
+    failed
+fi
+
+cd ..
+echo File name: $(ls output) >> info.txt
